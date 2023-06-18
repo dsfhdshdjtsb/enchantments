0077d1fa15ba0169e64af83346b649fb8f654ee6
SzczurekYT <67533827+SzczurekYT@users.noreply.github.com>
2023-06-18 09:26:47 +0200

    Fixe quilt incompatibility
    Fixes quilt incompatibility and removes one line of dead code

--- src/main/java/com/dsfhdshdjtsb/CombatEnchants/mixin/CenchantsBowItemMixin.java
+++ src/main/java/com/dsfhdshdjtsb/CombatEnchants/mixin/CenchantsBowItemMixin.java
@@ -3,25 +3,19 @@ package com.dsfhdshdjtsb.CombatEnchants.mixin;
 import com.dsfhdshdjtsb.CombatEnchants.CombatEnchants;
 import net.minecraft.entity.LivingEntity;
 import net.minecraft.entity.effect.StatusEffectInstance;
-import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.item.BowItem;
 import net.minecraft.item.ItemStack;
-import net.minecraft.util.Hand;
-import net.minecraft.util.TypedActionResult;
 import net.minecraft.world.World;
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Redirect;
+import org.spongepowered.asm.mixin.injection.ModifyVariable;
 
 @Mixin(BowItem.class)
 public abstract class CenchantsBowItemMixin {
 
-    @Shadow public abstract TypedActionResult<ItemStack> use(World world, PlayerEntity user, Hand hand);
-
     //thank you to Rubydesic on the fabric mc discord for the help
-    @Redirect(at = @At(value="INVOKE", target = "Lnet/minecraft/item/BowItem;getPullProgress(I)F"), method = "onStoppedUsing"  )
-    public float onStoppedUsing(int useTicks, ItemStack stack, World world, LivingEntity user, int remainingUseTicks)
+    @ModifyVariable(at = @At("STORE"), method = "onStoppedUsing", ordinal = 0 )
+    public float onStoppedUsing(float value, ItemStack stack, World world, LivingEntity user)
     {
         StatusEffectInstance barrageEffectInstance = user.getStatusEffect(CombatEnchants.BARRAGE_EFFECT);
         if(barrageEffectInstance != null)
@@ -33,6 +27,6 @@ public abstract class CenchantsBowItemMixin {
             }
             return 1;
         }
-        return BowItem.getPullProgress(useTicks);
+        return value;
     }
 }
